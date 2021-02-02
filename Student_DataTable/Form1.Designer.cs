f1ef1a7be949201e8cc1247ed9f79caa15544aaa
HUIKIM0 <fehs123@naver.com>
2021-02-02 19:11:48 +0900

    update

--- Student_DataTable/Form1.Designer.cs
+++ Student_DataTable/Form1.Designer.cs
@@ -30,29 +30,31 @@ namespace Student_DataTable
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.cboxRegClass = new System.Windows.Forms.ComboBox();
-            this.tboxRegName = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.rdoRegMale = new System.Windows.Forms.RadioButton();
-            this.rdoRegFemale = new System.Windows.Forms.RadioButton();
-            this.tboxRegRef = new System.Windows.Forms.TextBox();
             this.btnReg = new System.Windows.Forms.Button();
-            this.btnViewDataDel = new System.Windows.Forms.Button();
-            this.cboxViewClass = new System.Windows.Forms.ComboBox();
+            this.tboxRegRef = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdoRegFemale = new System.Windows.Forms.RadioButton();
+            this.rdoRegMale = new System.Windows.Forms.RadioButton();
+            this.tboxRegName = new System.Windows.Forms.TextBox();
+            this.cboxRegClass = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dgViewInfo = new System.Windows.Forms.DataGridView();
+            this.cboxViewClass = new System.Windows.Forms.ComboBox();
+            this.btnViewDataDel = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgViewInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnModify);
             this.groupBox1.Controls.Add(this.btnReg);
             this.groupBox1.Controls.Add(this.tboxRegRef);
             this.groupBox1.Controls.Add(this.panel1);
@@ -64,73 +66,27 @@ namespace Student_DataTable
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(766, 188);
+            this.groupBox1.Size = new System.Drawing.Size(766, 236);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "등록하기";
             // 
-            // groupBox2
+            // btnReg
             // 
-            this.groupBox2.Controls.Add(this.dgViewInfo);
-            this.groupBox2.Controls.Add(this.cboxViewClass);
-            this.groupBox2.Controls.Add(this.btnViewDataDel);
-            this.groupBox2.Location = new System.Drawing.Point(12, 228);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(766, 305);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "내용확인";
+            this.btnReg.Location = new System.Drawing.Point(288, 175);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(98, 27);
+            this.btnReg.TabIndex = 8;
+            this.btnReg.Text = "등록";
+            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
             // 
-            // label1
+            // tboxRegRef
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "반:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "이름:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "성별:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 148);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 15);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "특기:";
-            // 
-            // cboxRegClass
-            // 
-            this.cboxRegClass.FormattingEnabled = true;
-            this.cboxRegClass.Location = new System.Drawing.Point(79, 34);
-            this.cboxRegClass.Name = "cboxRegClass";
-            this.cboxRegClass.Size = new System.Drawing.Size(121, 23);
-            this.cboxRegClass.TabIndex = 4;
-            // 
-            // tboxRegName
-            // 
-            this.tboxRegName.Location = new System.Drawing.Point(79, 73);
-            this.tboxRegName.Name = "tboxRegName";
-            this.tboxRegName.Size = new System.Drawing.Size(121, 25);
-            this.tboxRegName.TabIndex = 5;
+            this.tboxRegRef.Location = new System.Drawing.Point(79, 144);
+            this.tboxRegRef.Name = "tboxRegRef";
+            this.tboxRegRef.Size = new System.Drawing.Size(307, 25);
+            this.tboxRegRef.TabIndex = 7;
             // 
             // panel1
             // 
@@ -140,17 +96,6 @@ namespace Student_DataTable
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(307, 27);
             this.panel1.TabIndex = 6;
-            // 
-            // rdoRegMale
-            // 
-            this.rdoRegMale.AutoSize = true;
-            this.rdoRegMale.Location = new System.Drawing.Point(0, 3);
-            this.rdoRegMale.Name = "rdoRegMale";
-            this.rdoRegMale.Size = new System.Drawing.Size(43, 19);
-            this.rdoRegMale.TabIndex = 0;
-            this.rdoRegMale.TabStop = true;
-            this.rdoRegMale.Text = "남";
-            this.rdoRegMale.UseVisualStyleBackColor = true;
             // 
             // rdoRegFemale
             // 
@@ -163,39 +108,83 @@ namespace Student_DataTable
             this.rdoRegFemale.Text = "여";
             this.rdoRegFemale.UseVisualStyleBackColor = true;
             // 
-            // tboxRegRef
+            // rdoRegMale
             // 
-            this.tboxRegRef.Location = new System.Drawing.Point(79, 144);
-            this.tboxRegRef.Name = "tboxRegRef";
-            this.tboxRegRef.Size = new System.Drawing.Size(307, 25);
-            this.tboxRegRef.TabIndex = 7;
+            this.rdoRegMale.AutoSize = true;
+            this.rdoRegMale.Location = new System.Drawing.Point(0, 3);
+            this.rdoRegMale.Name = "rdoRegMale";
+            this.rdoRegMale.Size = new System.Drawing.Size(43, 19);
+            this.rdoRegMale.TabIndex = 0;
+            this.rdoRegMale.TabStop = true;
+            this.rdoRegMale.Text = "남";
+            this.rdoRegMale.UseVisualStyleBackColor = true;
             // 
-            // btnReg
+            // tboxRegName
             // 
-            this.btnReg.Location = new System.Drawing.Point(407, 144);
-            this.btnReg.Name = "btnReg";
-            this.btnReg.Size = new System.Drawing.Size(98, 27);
-            this.btnReg.TabIndex = 8;
-            this.btnReg.Text = "등록";
-            this.btnReg.UseVisualStyleBackColor = true;
-            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
+            this.tboxRegName.Location = new System.Drawing.Point(79, 73);
+            this.tboxRegName.Name = "tboxRegName";
+            this.tboxRegName.Size = new System.Drawing.Size(121, 25);
+            this.tboxRegName.TabIndex = 5;
             // 
-            // btnViewDataDel
+            // cboxRegClass
             // 
-            this.btnViewDataDel.Location = new System.Drawing.Point(654, 23);
-            this.btnViewDataDel.Name = "btnViewDataDel";
-            this.btnViewDataDel.Size = new System.Drawing.Size(106, 27);
-            this.btnViewDataDel.TabIndex = 9;
-            this.btnViewDataDel.Text = "삭제";
-            this.btnViewDataDel.UseVisualStyleBackColor = true;
+            this.cboxRegClass.FormattingEnabled = true;
+            this.cboxRegClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxRegClass.Location = new System.Drawing.Point(79, 34);
+            this.cboxRegClass.Name = "cboxRegClass";
+            this.cboxRegClass.Size = new System.Drawing.Size(121, 23);
+            this.cboxRegClass.TabIndex = 4;
             // 
-            // cboxViewClass
+            // label4
             // 
-            this.cboxViewClass.FormattingEnabled = true;
-            this.cboxViewClass.Location = new System.Drawing.Point(6, 26);
-            this.cboxViewClass.Name = "cboxViewClass";
-            this.cboxViewClass.Size = new System.Drawing.Size(121, 23);
-            this.cboxViewClass.TabIndex = 9;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "특기:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "성별:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "이름:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "반:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgViewInfo);
+            this.groupBox2.Controls.Add(this.cboxViewClass);
+            this.groupBox2.Controls.Add(this.btnViewDataDel);
+            this.groupBox2.Location = new System.Drawing.Point(12, 255);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(766, 305);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "내용확인";
             // 
             // dgViewInfo
             // 
@@ -207,20 +196,53 @@ namespace Student_DataTable
             this.dgViewInfo.Size = new System.Drawing.Size(754, 242);
             this.dgViewInfo.TabIndex = 10;
             // 
+            // cboxViewClass
+            // 
+            this.cboxViewClass.FormattingEnabled = true;
+            this.cboxViewClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxViewClass.Location = new System.Drawing.Point(6, 26);
+            this.cboxViewClass.Name = "cboxViewClass";
+            this.cboxViewClass.Size = new System.Drawing.Size(121, 23);
+            this.cboxViewClass.TabIndex = 9;
+            this.cboxViewClass.SelectedIndexChanged += new System.EventHandler(this.cboxViewClass_SelectedIndexChanged);
+            // 
+            // btnViewDataDel
+            // 
+            this.btnViewDataDel.Location = new System.Drawing.Point(654, 23);
+            this.btnViewDataDel.Name = "btnViewDataDel";
+            this.btnViewDataDel.Size = new System.Drawing.Size(106, 27);
+            this.btnViewDataDel.TabIndex = 9;
+            this.btnViewDataDel.Text = "삭제";
+            this.btnViewDataDel.UseVisualStyleBackColor = true;
+            this.btnViewDataDel.Click += new System.EventHandler(this.btnViewDataDel_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(392, 175);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(98, 27);
+            this.btnModify.TabIndex = 9;
+            this.btnModify.Text = "수정";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 545);
+            this.ClientSize = new System.Drawing.Size(800, 576);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "학생부";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgViewInfo)).EndInit();
             this.ResumeLayout(false);
 
@@ -244,6 +266,7 @@ namespace Student_DataTable
         private System.Windows.Forms.DataGridView dgViewInfo;
         private System.Windows.Forms.ComboBox cboxViewClass;
         private System.Windows.Forms.Button btnViewDataDel;
+        private System.Windows.Forms.Button btnModify;
     }
 }
 
--- Student_DataTable/Form1.cs
+++ Student_DataTable/Form1.cs
@@ -29,41 +29,24 @@ namespace Student_DataTable
            2-2. 존재한다 -> 기존 DataTable 가져온다 */
         private void btnReg_Click(object sender, EventArgs e)
         {
+            bool CheckIsTable = false;  
 
-            /* DataTable에 DataRow 생성 */
-            DataRow row = dt.NewRow();
 
-            row["Name"] = tboxRegName.Text;
-
-            if (rdoRegMale.Checked)
-            {
-                row["Sex"] = "남자";
-            }
-            else if (rdoRegFemale.Checked)
-            {
-                row["Sex"] = "여자";
-            }
-
-            row["Ref"] = tboxRegRef.Text;
-
-            bool CheckIsTable = false;
-
-            // DataSet의 에 해당하는 DataTable이 있는지
+            // DataSet의 에 넣어준 값에 해당하는 DataTable이 있는지. 있으면 true
             if (ds.Tables.Contains(cboxRegClass.Text))
             {
                 CheckIsTable = true;
             }
 
-
-            /* 기존 DataTable이 없다 ->생성*/
+            /* 넣어준 값이 기존의 DataTable이 없다! 여기로 옴 ->생성*/
             if (!CheckIsTable)
             {
                 dt = new DataTable(cboxRegClass.Text);
 
                 //DataColumn 생성
-                DataColumn colName = new DataColumn("Name", typeof(string));
-                DataColumn colSex = new DataColumn("Sex", typeof(string));
-                DataColumn colRef = new DataColumn("Ref", typeof(string));
+                DataColumn colName = new DataColumn("NAME", typeof(string));
+                DataColumn colSex = new DataColumn("SEX", typeof(string));
+                DataColumn colRef = new DataColumn("REF", typeof(string));
 
                 dt.Columns.Add(colName);
                 dt.Columns.Add(colSex);
@@ -75,6 +58,88 @@ namespace Student_DataTable
             {
                 dt = ds.Tables[cboxRegClass.Text];
             }
+
+
+            /* ★ DataTable에 DataRow 생성 */
+            DataRow row = dt.NewRow();
+
+            row["NAME"] = tboxRegName.Text;
+
+            if (rdoRegMale.Checked)
+            {
+                row["SEX"] = "남자";
+            }
+            else if (rdoRegFemale.Checked)
+            {
+                row["SEX"] = "여자";
+            }
+
+            row["Ref"] = tboxRegRef.Text;
+
+
+            if (CheckIsTable)
+            {
+                ds.Tables[cboxRegClass.Text].Rows.Add(row); // DataSet에 해당 DataTalbe이 있을 경우 기존 Table에 Row를 추가한다
+            }
+            else
+            {
+                dt.Rows.Add(row);   //신규로 작성한 DataTable에 Row를 등록하고
+                ds.Tables.Add(dt);   //등록한 DataTable을 DataSet에 넣는다
+            }
+
+
+        }
+
+        /* Data 삭제 버튼. 맨 왼쪽 칼럼의 번호 클릭 후 삭제버튼 눌러야 삭제됨  */
+        private void btnViewDataDel_Click(object sender, EventArgs e)
+        {
+            int iSelectRow = dgViewInfo.SelectedRows[0].Index;  // dgViewInfo에 보여지는 값 중 선택한 값의 첫번째 Row번호를 가져온다
+
+            //DataSet에 있는 DataTable. 추출한 Index값을 이용해 Row삭제
+            ds.Tables[cboxViewClass.Text].Rows.RemoveAt(iSelectRow);
+            cboxViewClass_SelectedIndexChanged(this, null);
+
+        }
+
+        private void cboxViewClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgViewInfo.DataSource = ds.Tables[cboxViewClass.Text];
+
+
+            // DatagridView Cell 정렬 및 Number를 적용
+            foreach (DataGridViewRow oRow in dgViewInfo.Rows)
+            {
+                oRow.HeaderCell.Value = oRow.Index.ToString();
+            }
+            dgViewInfo.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+
+        }
+
+
+        /* 수정하기 */
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            //이름이 적혀있으면
+            if (!string.IsNullOrEmpty(tboxRegName.Text))
+            {
+                foreach (DataRow ditem in ds.Tables[cboxRegClass.Text].Rows)   //한줄씩 Row 형태로 뽑기
+                {
+                    if (ditem["NAME"].Equals(tboxRegName.Text))
+                    { 
+                        if (rdoRegFemale.Checked)
+                        {
+                            ditem["SEX"] = "여자";
+                        }
+                        else if (rdoRegMale.Checked)
+                        {
+                            ditem["SEX"] = "남자";
+                        }
+
+                        ditem["REF"] = tboxRegRef.Text;
+                    }
+                }
+            }
+            cboxViewClass_SelectedIndexChanged(this, null);
         }
     }
 }
